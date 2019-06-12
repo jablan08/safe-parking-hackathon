cad680cdaf216ee56b1b70ba8708b510ab052aef
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-06-12 10:56:23 -0700

    add to git

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 export default class Register extends Component {
+    submit = () => {
+        console.log("clicked")
+    }
     render() {
 
         return (
@@ -9,7 +12,7 @@ export default class Register extends Component {
                     <input type='tex' value="name"></input>
                     <input type='email' value='email'></input>
                     <input type='text' value='email'></input>
-                    <button type='submit'></button>
+                    <button onClick={this.submit} type='submit'></button>
                 </form>
             </div>
         )

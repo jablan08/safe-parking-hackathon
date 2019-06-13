59863aa18a05e37a116150d73d281c117c769817
Jablan <joshuaablan@gmail.com>
2019-06-13 16:27:42 -0700

    add to heroku

--- src/components/NewResource/NewResource.jsx
+++ src/components/NewResource/NewResource.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { Redirect } from "react-router-dom";
+import * as routes from "../../constants/routes";
 import Geocode from "react-geocode"
 import styled from "styled-components";
 
@@ -117,72 +119,78 @@ class NewResource extends Component{
   render(){
       
       return(
-      
-          <NewForm onSubmit={(e)=>this.handleSubmit(e)}>
-            <div className="top-form">
+          <>
+          {
+            this.state.newResource.resource
+            ?  <Redirect to={routes.ROOT}/>
+            :
+            <NewForm onSubmit={(e)=>this.handleSubmit(e)}>
+              <div className="top-form">
 
-              <h1>Add Resource</h1>
-              <img src="/images/addphoto.png" alt=''/>
-            </div>
-            {/* <label className="label-tag" htmlFor="servicePlanningArea">Service Planning Area:</label>
-            <input type='text' name="servicePlanningArea"
-          onChange={this.handleChange} autoComplete="off"/><br/>   */}
-            <label className="label-tag" htmlFor="operator">Name:</label>
-            <input type='text' name="operator"
-              onChange={this.handleChange} autoComplete="off" placeholder="Enter Name"/><br/>  
-            {/* <label className="label-tag" htmlFor="website">Website:</label> 
-            <input type='text' name="website"
-          onChange={this.handleChange} autoComplete="off"/><br/>   */}
-             <label className="label-tag" htmlFor="address">Location:</label>
-            <input type='text' name="address"
-              onChange={this.handleChange} autoComplete="off" placeholder="Enter Location" /><br/>  
-          <label className="label-tag" htmlFor="resource">Category:</label>
-          <input type='text' name="resource"
-            onChange={this.handleChange} autoComplete="off" placeholder="Category"/><br/>
-             {/* <label className="label-tag" htmlFor="email">Email:</label>
-            <input type='text' name="email"
-              onChange={this.handleChange} autoComplete="off"/><br/>   */}
-            <label className="label-tag" htmlFor="phone">Phone (XXX)-XXX-XXXX:</label>
-            <input type='text' name="phone" placeholder="(XXX)-XXX-XXXX"
-              onChange={this.handleChange} autoComplete="off" /><br/>  
-            <label className="label-tag" htmlFor="hoursOfOperation">Hours Of Operation:</label>
-            <label className="label-tag" htmlFor="monday">Monday </label>
-            <input type='text' name="hoursOfOperation.m"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM" /> 
-            <label className="label-tag" htmlFor="tuesday">Tuesday</label>  
-            <input type='text' name="hoursOfOperation.t"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/> 
-            <label className="label-tag" htmlFor="wednesday">Wednesday</label>   
-            <input type='text' name="hoursOfOperation.w"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/> 
-            <label className="label-tag" htmlFor="thursday">Thursday</label>  
-            <input type='text' name="hoursOfOperation.th"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
-            <label className="label-tag" htmlFor="friday">Friday</label>  
-            <input type='text' name="hoursOfOperation.f"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
-            <label className="label-tag" htmlFor="saturday">Saturday</label> 
-            <input type='text' name="hoursOfOperation.sat"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
-            <label className="label-tag" htmlFor="sunday">Sunday</label>  
-            <input type='text' name="hoursOfOperation.sun"
-              onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
-            {/* <label className="label-tag" htmlFor="walkInsAllowed">Walk Ins Allowed:</label>
-            <input type='checkbox' name="walkInsAllowed"
-              onChange={this.handleChange} autoComplete="off"/><br/>  
-            <label className="label-tag" htmlFor="operator">Eligibility Requirements:</label>
-            <input type='text' name="eligibilityRequirements"
-              onChange={this.handleChange} autoComplete="off"/><br/>  
-            <label className="label-tag" htmlFor="operator">Population Notes:</label>
-            <input type='text' name="populationNotes"
-              onChange={this.handleChange} autoComplete="off"/><br/>   */}
-            {/* <label className="label-tag" htmlFor="operator">Notes:</label>
-            <input type='text' name="notes"
-              onChange={this.handleChange} autoComplete="off" placeholder=""/><br/>   */}
-              
-            <button type="submit" className="button-submit"> Add </button> <br/>
-              
-          </NewForm>
+                <h1>Add Resource</h1>
+                <img src="/images/addphoto.png" alt=''/>
+              </div>
+              {/* <label className="label-tag" htmlFor="servicePlanningArea">Service Planning Area:</label>
+              <input type='text' name="servicePlanningArea"
+            onChange={this.handleChange} autoComplete="off"/><br/>   */}
+              <label className="label-tag" htmlFor="operator">Name:</label>
+              <input type='text' name="operator"
+                onChange={this.handleChange} autoComplete="off" placeholder="Enter Name"/><br/>  
+              {/* <label className="label-tag" htmlFor="website">Website:</label> 
+              <input type='text' name="website"
+            onChange={this.handleChange} autoComplete="off"/><br/>   */}
+              <label className="label-tag" htmlFor="address">Location:</label>
+              <input type='text' name="address"
+                onChange={this.handleChange} autoComplete="off" placeholder="Enter Location" /><br/>  
+            <label className="label-tag" htmlFor="resource">Category:</label>
+            <input type='text' name="resource"
+              onChange={this.handleChange} autoComplete="off" placeholder="Category"/><br/>
+              {/* <label className="label-tag" htmlFor="email">Email:</label>
+              <input type='text' name="email"
+                onChange={this.handleChange} autoComplete="off"/><br/>   */}
+              <label className="label-tag" htmlFor="phone">Phone (XXX)-XXX-XXXX:</label>
+              <input type='text' name="phone" placeholder="(XXX)-XXX-XXXX"
+                onChange={this.handleChange} autoComplete="off" /><br/>  
+              <label className="label-tag" htmlFor="hoursOfOperation">Hours Of Operation:</label>
+              <label className="label-tag" htmlFor="monday">Monday </label>
+              <input type='text' name="hoursOfOperation.m"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM" /> 
+              <label className="label-tag" htmlFor="tuesday">Tuesday</label>  
+              <input type='text' name="hoursOfOperation.t"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/> 
+              <label className="label-tag" htmlFor="wednesday">Wednesday</label>   
+              <input type='text' name="hoursOfOperation.w"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/> 
+              <label className="label-tag" htmlFor="thursday">Thursday</label>  
+              <input type='text' name="hoursOfOperation.th"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
+              <label className="label-tag" htmlFor="friday">Friday</label>  
+              <input type='text' name="hoursOfOperation.f"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
+              <label className="label-tag" htmlFor="saturday">Saturday</label> 
+              <input type='text' name="hoursOfOperation.sat"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
+              <label className="label-tag" htmlFor="sunday">Sunday</label>  
+              <input type='text' name="hoursOfOperation.sun"
+                onChange={this.handleChange} autoComplete="off" placeholder="Ex: 8:00AM - 10:00AM"/>
+              {/* <label className="label-tag" htmlFor="walkInsAllowed">Walk Ins Allowed:</label>
+              <input type='checkbox' name="walkInsAllowed"
+                onChange={this.handleChange} autoComplete="off"/><br/>  
+              <label className="label-tag" htmlFor="operator">Eligibility Requirements:</label>
+              <input type='text' name="eligibilityRequirements"
+                onChange={this.handleChange} autoComplete="off"/><br/>  
+              <label className="label-tag" htmlFor="operator">Population Notes:</label>
+              <input type='text' name="populationNotes"
+                onChange={this.handleChange} autoComplete="off"/><br/>   */}
+              {/* <label className="label-tag" htmlFor="operator">Notes:</label>
+              <input type='text' name="notes"
+                onChange={this.handleChange} autoComplete="off" placeholder=""/><br/>   */}
+                
+              <button type="submit" className="button-submit"> Add </button> <br/>
+                
+            </NewForm>
+          }
+          </>
       )
 
   }
